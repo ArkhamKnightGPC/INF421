6bfa863181fc7208970838f91a93255b04c205f7
Gabriel Pereira de Carvalho <nazter57@usp.br>
2023-12-26 15:12:49 -0300

    :memo: task 1 done

--- Individual.nim
+++ Individual.nim
@@ -1,21 +1,40 @@
 # Individual data type
 
 type
-    Individual* = object
+    Individual* = ref object
         #[ represents candidate solutions x = (x1, ..., xn) ]#
         size*: int
-        bits*: int
+        bits*: seq[int]
+
+proc newIndividual(size: int): Individual =
+    #[ constructor for new Individual ]#
+
+    #number of integers necessary to represent all xi's
+    let necessaryIntegers = (size + 31) div 32
+
+    result = new(Individual)
+    result.size = size
+    result.bits = newSeq[int](necessaryIntegers)
+
+    return result
 
 proc get*(individual: Individual, idx: int): int =
     #[ get bit at index idx ]#
-    return
+    let testBit = individual.bits[idx div 32] and (1 shl (idx mod 32))
+
+    if testBit > 0:
+        return 1
+    else:
+        return 0
 
 proc set*(individual: Individual, idx: int): void =
     #[ set bit at index idx to 1 ]#
+    individual.bits[idx div 32] = individual.bits[idx div 32] or (1 shl (idx mod 32)) 
     return
 
 proc reset*(individual: Individual, idx: int): void =
     #[ set bit at index idx to 0 ]#
+    individual.bits[idx div 32] = individual.bits[idx div 32] and not (1 shl (idx mod 32))
     return
 
 proc flip*(individual: Individual, idx: int): void =
